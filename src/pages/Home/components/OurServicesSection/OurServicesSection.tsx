9e8588f6140c9b87cb3876cbd90e1add8173a69a
NIa-Frontend <frontniamedia@gmail.com>
2024-06-02 13:03:05 +0300

    done

--- src/pages/Home/components/OurServicesSection/OurServicesSection.tsx
+++ src/pages/Home/components/OurServicesSection/OurServicesSection.tsx
@@ -48,7 +48,7 @@ const OurServicesSection = () => {
     <>
       <section
         id="our-services"
-        className="h-[]  py-7 px-[40px] sm:px-10 lg:px-[100px]  overflow-hidden"
+        className="  py-7 px-[40px] sm:px-10 lg:px-[100px]  overflow-hidden"
       >
         <div className="layerService"></div>
         <HeadingTitle headerDark="our services" classes={"text-white 	"} />
@@ -62,40 +62,42 @@ const OurServicesSection = () => {
             lorem ibsum donetlorem ibsum donet lorem ibsum donetlorem{" "}
           </Text>
         </div>
-        <div className="bg-red-200 ourServicesCardsContainer mt-[32px] mb-[24px] bg-re-200 ">
-          <div className="bg-sky-400 container ServiceCard pb-[300px]  flex flex-col md:flex-row flex-wrap gap-3 xl:gap-[56px]  justify-center items-center mb-2">
+        <div className=" ourServicesCardsContainer mt-[32px] mb-[24px] bg-re-200 ">
+          <div className=" container ServiceCard xl:pb-24 flex flex-col md:flex-row flex-wrap gap-3 xl:gap-[55px]  justify-center items-center mb-2">
             {cardsData?.map((cardData, index) => {
               const { icon, header, url } = cardData;
               return (
                 <>
-                  <div
-                    key={index}
-                    className="relative  card rounded-2xl bluer   w-[210px]  xl:w-[376px] h-[170px] xl:h-auto mb-[18px] "
-                  >
-                    <div className="flex  rounded-2xl bluer border  w-[210px] xl:w-[376px] xl:h-[214px] h-[108px] mb-[18px] ">
-                      <span className=" py-5 px-2">
-                        <img
-                          src={serviceCardHeaderIcon}
-                          className="w-[13px] xl:w-[24px]"
-                        />
-                      </span>
-                      <h2 className="text-white py-4 px-2 text-[16px] xl:text-[24px]">
-                        {header}
-                      </h2>
-                    </div>
-                    {/*  */}
-                    <div className="absolute rounded-2xl bluer  translate-x-[11%]  max-xl:-translate-y-[115%] xl:top-[97px]  w-[170px] xl:w-[320px] bg-[#e2e2e24b] h-[70px] mb-[18px] "></div>
-                    <div className="absolute  h-[113px]  top-[58px]  xl:w-[376px] xl:h-[224px] xl:top-[115px] ">
-                      <img
-                        src={cardServicesCategory2}
-                        className="w-full object-fill"
-                      />
-                    </div>
-                    <s
-                      className={`circleService absolute rounded-full  right-0 translate-y-[10px] xl:translate-y-[190%] xl:w-[70px] xl:h-[70px]  `}
+                  <div className="cards xl:mt-24">
+                    <div
+                      key={index}
+                      className="relative  card rounded-2xl bluer   w-[210px]  xl:w-[376px] h-[170px] xl:h-auto mb-[18px] "
                     >
-                      <FaArrowRightLong className="-rotate-45 text-white w-[25.5px] h-[25.5px] xl:bottom-0" />
-                    </s>
+                      <div className="flex  rounded-2xl bluer border  w-[210px] xl:w-[376px] xl:h-[214px] h-[108px] mb-[18px] ">
+                        <span className=" py-5 px-2">
+                          <img
+                            src={serviceCardHeaderIcon}
+                            className="w-[13px] xl:w-[24px]"
+                          />
+                        </span>
+                        <h2 className="text-white py-4 px-2 text-[16px] xl:text-[24px]">
+                          {header}
+                        </h2>
+                      </div>
+                      {/*  */}
+                      <div className="absolute rounded-2xl bluer  translate-x-[11%]  max-xl:-translate-y-[115%] xl:top-[97px]  w-[170px] xl:w-[320px] bg-[#e2e2e24b] h-[70px] mb-[18px] "></div>
+                      <div className="absolute  h-[113px]  top-[58px]  xl:w-[376px] xl:h-[224px] xl:top-[115px] ">
+                        <img
+                          src={cardServicesCategory2}
+                          className="w-full object-fill"
+                        />
+                      </div>
+                      <s
+                        className={`circleService absolute rounded-full  right-0 translate-y-[10px] xl:translate-y-[190%] xl:w-[70px] xl:h-[70px]  `}
+                      >
+                        <FaArrowRightLong className="-rotate-45 text-white w-[25.5px] h-[25.5px] xl:bottom-0" />
+                      </s>
+                    </div>
                   </div>
                 </>
               );
--- src/pages/Home/components/OurWorkSection/OurWorkSection.tsx
+++ src/pages/Home/components/OurWorkSection/OurWorkSection.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { Text } from "@chakra-ui/react";
+import { HeadingTitle } from "../../../../components/text";
 
 const OurWorkSection = () => {
   return (
-    <section className="container mx-auto py-8">
+    <section className="container mx-auto py-8 px-[18px] max-md:bg-[#FAD4AA]">
+      <div className="div text-wrap w-[30%] md:w-[40%] lg:w-[10%] xl:w-[30%]  mx-auto">
+        {" "}
+        <HeadingTitle
+          headerDark="What we have"
+          headerOrang="achieved"
+          classes=""
+        />
+      </div>
       <Text
         fontSize={{ base: "16px", md: "18px" }}
         marginBottom={{ base: "32px", md: "64px", lg: "100px" }}
@@ -13,23 +22,19 @@ const OurWorkSection = () => {
       </Text>
       <div className="relative">
         <div
-          className="backgroundLayer absolute bg-[#FAD4AA] w-full max-md:top-0 max-md:bottom-0 max-md:left-0 max-md:right-0 z-1 md:h-[261px] lg:h-[400px]
-                 md:top-1/2 md:left-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2"
+          className="backgroundLayer absolute -left-10 md:bg-[#FAD4AA] w-screen max-md:top-0 max-md:bottom-0 max-md:left-0 max-md:right-0 z-1 md:h-[261px] lg:h-[400px]
+                 md:top-1/2 md:left-1/2  right-10 md:transform md:-translate-x-1/2 md:-translate-y-1/2"
         ></div>
-        <div className="ourWorksContainer bg-dark relative z-2 mx-[18px] md:mx-[24px] lg:mx-[11.111%]">
+        <div className="ourWorksContainer  relative z-2 mx-[18px] md:mx-[24px] lg:mx-[11.111%]">
           <div className="py-[24px] flex flex-col md:flex-row justify-between gap-4 xl:gap-[35px]">
             {/* order-1 */}
             <div className="inline-flex md:flex-col justify-center md:w-[31.8%] gap-4 xl:gap-[35px]">
-              <div className="max-md:w-[162px] w-full h-[140px] md:h-full lg:h-[305px] bg-slate-400 rounded-xl">
-                31%
-              </div>
+              <div className="max-md:w-[162px] w-full h-[140px] md:h-full lg:h-[305px] bg-slate-400 rounded-xl"></div>
               <div className="w-full md:w-full lg:w-full h-[140px] md:h-full lg:h-[305px] bg-slate-400 rounded-xl"></div>
             </div>
             {/* order-2 */}
-            <div className="flex-1 flex justify-center m-auto bg-red-500 h-full">
-              <div className="w-full md:w-full lg:w-full h-full bg-slate-400 rounded-xl">
-                aa order-2aa
-              </div>
+            <div className=" flex justify-center m-auto  h-full w-[280px] md:h-[326px] lg:w-[400px] lg:h-[630px] ">
+              <div className="w-full md:w-full  lg:w-full h-[140px] md:h-full bg-slate-400 rounded-xl"></div>
             </div>
             {/* order-3 */}
             <div className="flex md:flex-col justify-center gap-4 xl:gap-[35px] md:w-[31.8%]">
